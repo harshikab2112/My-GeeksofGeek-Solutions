c47e7b52af4833493259479f8ceba7d4b0ba799f
HARSHIKA BANSAL <86090916+harshikab2112@users.noreply.github.com>
2025-03-01 12:39:08 +0530

    Create Check Subset.py

---
+++ Python/Easy/Check Subset.py
@@ -0,0 +1,9 @@
+a = set(list(map(int, input().split())))
+b = set(list(map(int, input().split())))
+
+########### Write your code below ###############
+res = a.issubset(b)# Is a subset of b?
+
+########### Write your code above ###############
+# Print True or False
+print(res)
