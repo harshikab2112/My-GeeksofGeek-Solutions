1d20abf5d334e5b945a3e3db5cb5adac495dd528
HARSHIKA BANSAL <86090916+harshikab2112@users.noreply.github.com>
2025-03-01 12:55:31 +0530

    Create Separate Even Odd.py

---
+++ Python/Easy/Separate Even Odd.py
@@ -0,0 +1,8 @@
+def evenOdd(arr):
+    even = [i for i in arr if i%2==0]
+    odd = [i for i in arr if i%2!=0]
+
+    #Write your code below to append odd elements in numbers to odd list
+    #Write your code below to append even elements in numbers to even list
+    
+    return (even, odd)
