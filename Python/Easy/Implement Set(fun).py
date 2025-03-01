dca817a0d0678fb3ff309a7607fc5506c5846b02
HARSHIKA BANSAL <86090916+harshikab2112@users.noreply.github.com>
2025-03-01 13:55:16 +0530

    Create Implement Set(fun).py

---
+++ Python/Easy/Implement Set(fun).py
@@ -0,0 +1,16 @@
+def insert(s, element):
+    # Your code here
+    s.add(element)    
+
+# Function to remove element from set
+# No return and nothing to print
+def remove_from_set(s, element):
+    # Your code here
+    s.remove(element)
+    
+# Function to find sum of elements in set
+# return value should be there as sum
+def sum_set(s):
+    # Your code here
+    sum_s=sum(s)
+    return sum_s
