d796506fb8092d604815f33e541e8c8f42802755
HARSHIKA BANSAL <86090916+harshikab2112@users.noreply.github.com>
2025-03-02 21:14:07 +0530

    Create Append To List.py

---
+++ Python/Basic/Append To List.py
@@ -0,0 +1,10 @@
+def appendToList(a,b,c):
+    #Write the code below
+    #Create a list
+    mylist=[]
+    #Append a b c in order
+    mylist.append(a)
+    mylist.append(b)
+    mylist.append(c)
+    #Return the list
+    return mylist
