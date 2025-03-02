94703c1d534ca02b9b6ee752d27cbabb2c1b6271
HARSHIKA BANSAL <86090916+harshikab2112@users.noreply.github.com>
2025-03-02 16:49:59 +0530

    Create Set in Python - II.py

---
+++ Python/Easy/Set in Python - II.py
@@ -0,0 +1,17 @@
+def common_in_set(a, b):
+    # Your code here
+    return a.intersection(b)
+
+# Function to find difference in sets
+# Should return the difference in sets
+def diff(a, b):
+    # Your code here
+    return a.difference(b)
+
+
+
+# Function to find union of sets
+# Should return the union of sets
+def all_in_set(a, b):
+    # Your code here
+    return a.union(b)
