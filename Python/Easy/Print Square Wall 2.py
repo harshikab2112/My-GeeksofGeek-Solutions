181b11ae19ff201c03e002d3c084917866ce73fe
HARSHIKA BANSAL <86090916+harshikab2112@users.noreply.github.com>
2025-03-02 16:31:16 +0530

    Create Print Square Wall 2.py

---
+++ Python/Easy/Print Square Wall 2.py
@@ -0,0 +1,10 @@
+def squareWall(s):
+    
+    #Complete the codes below 
+    #Replace .... with your own code
+    for i in range(s):
+        
+        for j in range(s):
+            print("*",end=" ")
+        #This print below gives new line, so you don't have to.
+        print()
