036e33d4c0bb9c7b879c2ab29a41077cfd9a9448
HARSHIKA BANSAL <86090916+harshikab2112@users.noreply.github.com>
2025-03-02 17:03:26 +0530

    Create Sum of N Numbers.py

---
+++ Python/Basic/Sum of N Numbers.py
@@ -0,0 +1,8 @@
+n = int(input())
+
+########### Write your code below ###############
+# Compute the sum of first n natural numbers
+ans = n*(n+1)//2
+########### Write your code above ###############
+
+print(ans)
