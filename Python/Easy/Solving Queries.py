48ba663bd1eb72c9fc22fd0bb8e1820b757cd52d
HARSHIKA BANSAL <86090916+harshikab2112@users.noreply.github.com>
2025-03-02 16:24:13 +0530

    Create Solving Queries.py

---
+++ Python/Easy/Solving Queries.py
@@ -0,0 +1,18 @@
+a = list(map(int, input().split()))
+b = list(map(str, input().split()))
+query = list(map(int, input().split()))
+dict = {}
+for i in range(len(a)):
+    dict[a[i]] = b[i]
+        
+ans = []
+for key in range(len(query)):
+    ########### Write your code below ###############
+    val = dict.get(query[key], "None") 
+    ########### Write your code above ###############
+    
+    # append to ans
+    ans.append(val)
+
+# Print ans
+print(*ans, sep='\n')
