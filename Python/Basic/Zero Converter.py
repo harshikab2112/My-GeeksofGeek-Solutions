ca32e55ff5668adcf5d529bfcaf189b21ba4dcb2
HARSHIKA BANSAL <86090916+harshikab2112@users.noreply.github.com>
2025-03-04 12:34:35 +0530

    Create Zero Converter.py

---
+++ Python/Basic/Zero Converter.py
@@ -0,0 +1,16 @@
+def pos(n):
+    ## Write the code
+    if n>0:
+        i=n-1
+        while i>=0:
+            print(i,end=" ")
+            i-=1
+    
+    
+def neg(n):
+    ##Write the code
+    if n<0:
+        i=n
+        while i<=0:
+            print(i,end=" ")
+            i+=1
