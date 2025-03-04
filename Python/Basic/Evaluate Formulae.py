bc572e71b2d531af256953ef22eaace834f73f8d
HARSHIKA BANSAL <86090916+harshikab2112@users.noreply.github.com>
2025-03-04 12:46:37 +0530

    Create Evaluate Formulae.py

---
+++ Python/Basic/Evaluate Formulae.py
@@ -0,0 +1,12 @@
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+
+########### Write your code below ###############
+#Complete the code below that stores the output of the expression
+res = ((a+b)//c)+d
+########### Write your code above ###############
+
+#This prints the output. Don't change it!
+print(res)
