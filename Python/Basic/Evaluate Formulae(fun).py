be45fd567b6c3be65fd88a1fc972b64b2833e1b3
HARSHIKA BANSAL <86090916+harshikab2112@users.noreply.github.com>
2025-03-04 12:45:12 +0530

    Create Evaluate Formulae(fun).py

---
+++ Python/Basic/Evaluate Formulae(fun).py
@@ -0,0 +1,7 @@
+def evalute(a, b, c, d):
+    #Complete the code below that stores the output of the expression
+    res = ((a+b)//c)+d
+    #Complete the code above that stores the output of the expression
+    
+    #This prints the output. Don't change it!
+    print(res)
