3cfb6ae5d54355829c5c4a7b708c4067648c2ad9
HARSHIKA BANSAL <86090916+harshikab2112@users.noreply.github.com>
2025-03-05 15:23:57 +0530

    Create Next Prime Number.py

---
+++ Python/Medium/Next Prime Number.py
@@ -0,0 +1,14 @@
+def checkPrime(num):
+    if num<2:
+        False
+    else:
+        for i in range(2,int(num**0.5)+1):
+            if num%i==0:
+                return False
+        return True
+
+def nextPrime(n):
+    number=n+1
+    while not checkPrime(number):
+        number+=1
+    return number
